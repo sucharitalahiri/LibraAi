b9fc6ddf6bd2dcf7642497818aab7ccc989085f2
Sucharita Lahiri <34596224+sucharitalahiri@users.noreply.github.com>
2026-06-07 22:13:13 +0530

    Update taskRoutes.js

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 const authMiddleware =
   require("../middleware/authMiddleware");
 
-const roleMiddleware =
-  require("../middleware/roleMiddleware");
+// const roleMiddleware =
+//   require("../middleware/roleMiddleware");
 
 const {
   createTask,
